71b1fdf7c0db8cccd2a641f4ffed4bc361f5b258
Alex Malkevich <malkevich.alex@gmail.com>
2015-12-13 07:46:04 +0400

    Fix e2e test

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -7,7 +7,7 @@ describe('my app', function() {
 
   it('should automatically redirect to / when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("#/");
+    expect(browser.getLocationAbsUrl()).toMatch("/");
   });
 
 
